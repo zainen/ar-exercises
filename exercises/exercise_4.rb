9cda0905198c8d63cb246c5998fe2f70cb605e5e
zainen <zainensuzuki@gmail.com>
2021-02-02 01:16:13 +0000

    ex4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,20 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+# 1. Borrowing and modifying the code from Exercise one, create 3 more stores:
+#   * Surrey (annual_revenue of 224000, carries women's apparel only)
+#   * Whistler (annual_revenue of 1900000 carries men's apparel only)
+#   * Yaletown (annual_revenue of 430000 carries men's and women's apparel)
+# 2. Using the `where` class method from Active Record, fetch (a collection of) only those stores that carry men's apparel. Assign the results to a variable `@mens_stores`.
+# 3. Loop through each of these stores and output their name and annual revenue on each line.
+# 4. Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.
+
+@store4 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+@store5 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+@store6 = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+@mens = Store.where(mens_apparel: true)
+puts @mens.count
+@mens.each do |store|
+  puts store.name
+end

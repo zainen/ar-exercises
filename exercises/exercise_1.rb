73f79344b5c4464ce1572a26048f399a3dd4c8f3
zainen <zainensuzuki@gmail.com>
2021-02-02 00:36:27 +0000

    insert into tables

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,23 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
+#   * Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+# * Richmond (annual_revenue of 1260000 carries women's apparel only)
+# * Gastown (annual_revenue of 190000 carries men's apparel only)
+
 # Your code goes below here ...
+
+class Store < ActiveRecord::Base
+end
+
+burnaby = Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+richmond =  Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+gastown = Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+# store.save
+# store = Store.new do |s|
+#   s.name = 'Burnaby'
+#   s.annual_revenue = 300000
+#   s.mens_apparel = true
+#   s.womens_apparel = true
+# end
+# store.save
